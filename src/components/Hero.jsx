70d3da630b073496a035dab460eb84192e2c49ef
ayoubDahb <ayoub.dahbi.sio@gmail.com>
2024-10-09 23:25:13 +0200

    about section and correction hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <img src={profilePic} alt="profilepic" />
+            {/* <img src={profilePic} alt="profilepic" /> */}
           </div>
         </div>
       </div>
