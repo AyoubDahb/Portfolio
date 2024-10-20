1e95777c8926d76472f58cd8a82dae9ce4f72418
AyoubDahb <157691365+AyoubDahb@users.noreply.github.com>
2024-10-20 23:44:32 +0200

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -11,7 +11,7 @@ export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer w
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
+    year: "2024 - Present",
     role: "Senior Full Stack Developer",
     company: "Google Inc.",
     description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
